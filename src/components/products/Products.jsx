7441c3de05f687a38c6b59e1e8c14dad4c17001a
léo <leo.poupet@eliberty.fr>
2020-10-23 09:53:04 +0200

    test changes gatsby

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Products = () => (
   <div>
-    La j'affiche tous mes produits de plongée;
+    La j'affiche tous mes produits de plongée
+    <br/>
+    ou d'autres truc genre le panier etc...
   </div>
 )
 
